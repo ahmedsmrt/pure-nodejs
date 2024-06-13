e9443c2c5a67c94b928ee197bc837c1faaf13d55
agasimdev <agasim@archeredu.com>
2024-06-12 23:51:28 -0400

    dev: created minimal dynamic web server with different file types to be served. I've also included error and request logging

---
+++ 05-pure-node-server/logEvents.js
@@ -0,0 +1,26 @@
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+// Common core modules
+const fs = require('fs');
+const fsPromises = fs.promises; // fsPromises is a built-in module that provides promise-based versions of the fs functions.
+const path = require('path');
+
+const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), 'yyyy-MM-dd\tHH:mm:ss')}`;
+
+    const logItem = `\nDate and time is: ${dateTime}\nID: ${uuid()}\nMessage: ${message}\n`;
+    console.log(logItem);
+
+    try {
+        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+            await fs.mkdirSync(path.join(__dirname, 'logs'));
+        }
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+module.exports = logEvents;
