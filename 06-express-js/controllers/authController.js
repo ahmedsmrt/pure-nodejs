55192ef3b45002bad2b17287899c9de8b2d401ba
agasimdev <agasim@archeredu.com>
2024-07-06 11:30:00 -0400

    feat: added simple jwt authorization logic with access token and refreshtoken to protect all of the routes in our employee endpoint

--- 06-express-js/controllers/authController.js
+++ 06-express-js/controllers/authController.js
@@ -4,18 +4,43 @@ const usersDB = {
 }
 const bcrypt = require('bcrypt');
 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const fsPromises = require('fs/promises')
+const path = require('path')
+
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
 
     const foundUser = usersDB.users.find(person => person.username === user);
-    if(!foundUser) return res.sendStatus(401);// Unauthorized
+    if (!foundUser) return res.sendStatus(401);// Unauthorized
     // Evaluate Password
     const match = await bcrypt.compare(pwd, foundUser.password);
-    if(match) {
-        res.json({ 'success': `User ${user} has been logged in.`})
-    }else {
-        res.sendStatus('401') 
+    if (match) {
+        // Creat JWT's
+        const accessToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '60000s' }
+        );
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+        //  Creates array of other users and saves refreshtoken with current user
+        const otherUsers = usersDB.users.filter(person => person.username !== foundUser)
+        const currentUser = { ...foundUser, refreshToken };
+        usersDB.setUsers([...otherUsers, currentUser])
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'models', 'users.json'),
+            JSON.stringify(usersDB.users)
+        )
+        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+        res.json({ accessToken })
+    } else {
+        res.sendStatus('401')
     }
 }
 
--- 06-express-js/server.js
+++ 06-express-js/server.js
@@ -5,6 +5,7 @@ const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions')
+const verifyJWT = require('./middleware/verifyJWT')
 
 const PORT = process.env.PORT || 3500;
 
@@ -29,6 +30,7 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));
 
 // Route Handlers
